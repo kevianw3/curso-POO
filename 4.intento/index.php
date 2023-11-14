4872a9920f0be441925245781df499116820d5aa
kevianWeky <basededatos@basededatos01.mppre.gob.ve>
2023-11-14 14:10:38 -0400

    Se agrego nueva carpeta

--- 4.intento/index.php
+++ 4.intento/index.php
@@ -24,15 +24,3 @@
         }
 
     }
-
-    $Keivna = new Persona("kevian" ,20);
-     echo $Keivna->Saludar("Hola soy");
-     echo $Keivna->EdadDePersona();
-
-     echo "<br><br>";
-    
-   $Armando = new Persona("Armando" ,19);
-    echo $Armando->Saludar("Mi nombre es");
-    echo $Armando->EdadDePersona();
-
-    echo "besos";
